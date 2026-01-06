7dba0d21a8628eab27adbe45856c4093f984df22
rushikasirigadde <rushika.nagarjuna@gmail.com>
2026-01-05 21:36:39 -0600

    updated changes

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,11 +8,11 @@ const experiences = [
     duration: 'Aug 2025 – Till Date',
     location: 'United States',
     highlights: [
-      'Architected a secure health application using Next.js,React and TypeScript, achieving a 35% increase in page-load speeds. '
-      'Leveraged Python based AI APIs to perform automated data validation on uploaded medical files, ensuring 99.9% accuracy in document categorization.'
-      'Engineered scalable Node.js file-upload pipelines and optimized MongoDB schemas to handle large-scale medical datasets with 100% integrity. '
-      'Orchestrated background tasks via AWS SNS/SQS, ensuring UI responsiveness during complex data validation.'
-      'Automated the CI/CD lifecycle using GitHub Actions and Jenkins, reducing manual deployment effort by 50%. '
+      'Architected a secure health application using Next.js,React and TypeScript, achieving a 35% increase in page-load speeds. ',
+      'Leveraged Python based AI APIs to perform automated data validation on uploaded medical files, ensuring 99.9% accuracy in document categorization.',
+      'Engineered scalable Node.js file-upload pipelines and optimized MongoDB schemas to handle large-scale medical datasets with 100% integrity. ',
+      'Orchestrated background tasks via AWS SNS/SQS, ensuring UI responsiveness during complex data validation.',
+      'Automated the CI/CD lifecycle using GitHub Actions and Jenkins, reducing manual deployment effort by 50%. ',
       'Optimized system reliability & uptime to 99.9% by implementing robust error-handling and monitoring protocols. '
     ]
   },
@@ -22,11 +22,11 @@ const experiences = [
     duration: 'Jun 2022 – May 2023',
     location: 'Hyderabad, Telangana, India',
     highlights: [
-     'Spearheaded the development of a high-performance Vue.js SPA, achieving a 70% increase in system throughput. '
-'Implemented real-time monitoring flows via Socket.io to track CRUD operations, reducing critical incident response times by 80%. '
-'Launched a performance-analytics dashboard integrated with JMeter and Node.js, enabling real-time load testing and stability assessment under high-concurrency procurement loads. '
-'Developed interactive, data-intensive visualizations using APACHE ECharts, converting complex engineering datasets into actionable executive-level insights. '
-'Optimized Real User Experience (RUX) monitoring via custom scripts, resulting in a 90% boost in user engagement. '
+     'Spearheaded the development of a high-performance Vue.js SPA, achieving a 70% increase in system throughput. ',
+'Implemented real-time monitoring flows via Socket.io to track CRUD operations, reducing critical incident response times by 80%. ',
+'Launched a performance-analytics dashboard integrated with JMeter and Node.js, enabling real-time load testing and stability assessment under high-concurrency procurement loads. ',
+'Developed interactive, data-intensive visualizations using APACHE ECharts, converting complex engineering datasets into actionable executive-level insights. ',
+'Optimized Real User Experience (RUX) monitoring via custom scripts, resulting in a 90% boost in user engagement. ',
 'Streamlined frontend-to-backend data flow by optimizing PostgreSQL queries and RESTful API consumption, reducing data retrieval latency by 40%.  '
     ]
   },
@@ -36,9 +36,9 @@ const experiences = [
     duration: 'Nov 2020 – Jun 2022',
     location: 'Hyderabad, Telangana, India',
     highlights: [
-      'Specialized in the WaveMaker platform for 1.5+ years, building enterprise Insurance Policy portals and extending core functionality through custom Angular components and TypeScript modules.' 
-'Designed modular UI components with React for logistics and claims processing, utilizing RESTful APIs to improve data retrieval speeds by 30% for over 5,000 internal users. '
-'Containerized legacy services using Docker and automated unit testing via GitHub Actions, leading to a 35% reduction in production-level defect.'
+      'Specialized in the WaveMaker platform for 1.5+ years, building enterprise Insurance Policy portals and extending core functionality through custom Angular components and TypeScript modules.' ,
+'Designed modular UI components with React for logistics and claims processing, utilizing RESTful APIs to improve data retrieval speeds by 30% for over 5,000 internal users. ',
+'Containerized legacy services using Docker and automated unit testing via GitHub Actions, leading to a 35% reduction in production-level defect.',
 'Ensured all UI/UX components met strict WCAG 2.1 accessibility and branding standards for global insurance clients.'
     ]
   },
@@ -48,8 +48,8 @@ const experiences = [
     duration: 'Jan 2019 – Jun 2019',
     location: 'Hyderabad, Telangana, India',
     highlights: [
-      'Rapidly developed web-based insurance policy prototypes using WaveMaker and JavaScript, streamlining the proof-of concept phase for new client acquisitions. '
-'Assisted in mapping & migrating legacy data structures into modern REST APIs, ensuring seamless data flow. '
+      'Rapidly developed web-based insurance policy prototypes using WaveMaker and JavaScript, streamlining the proof-of concept phase for new client acquisitions. ',
+'Assisted in mapping & migrating legacy data structures into modern REST APIs, ensuring seamless data flow. ',
 'Refined layouts using CSS3 & HTML5 Flexbox/Grid, improving cross-browser compatibility & user engagement by 15%.'
     ]
   }
